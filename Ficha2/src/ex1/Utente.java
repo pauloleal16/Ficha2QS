66bc0bdad17b329770c40cb322622e6b268a32e3
Paulo <Paulo@10.2.4.210>
2022-10-03 14:41:09 +0100

    Versão 2 - Construtor Get's e Set's

--- Ficha2/src/ex1/Utente.java
+++ Ficha2/src/ex1/Utente.java
@@ -63,64 +63,6 @@ public class Utente {
 		//this.imc = imc;
 	}
 	
-	
-	@Override
-	public String toString() {
-		
-		return "\n --> Utente: \n Nome=" + nome + ", \n Genero=" + genero + ", \n Idade=" + idade + ", \n Altura=" + altura + ", \n Peso="
-				+ peso + "\n IMC=" + imc;
-	}
-	
-// método calcular e definir imc
-	public void imc() {
-		
-		imc = peso/(altura*altura);
-		
-		System.out.printf ("IMC : " + imc + "\n");
-		
-		if (imc <= 19) {
-	        System.out.println ("Abaixo do peso normal");}
-			
-	    else
-	        if ((imc > 19) && (imc <=25)) {
-	            System.out.println ("Peso normal");}
-	    else        
-	        if ((imc > 25.0)&& (imc <=30)) {
-	            System.out.println("Excesso de peso");}
-				
-		else 
-			if ((imc > 30)){
-	            System.out.println("Obeso");
-	            }
-	}
 
-//método para avaliar o estado de saude do utente
-	public void estado(int p, float a) {
-		
-		double pesoIdeal = 0;
-		double limiteInf = 0;
-		double limiteSup = 0;
-		
-		imc = peso/(altura*altura);
-		
-		System.out.printf ("IMC : " + imc + "\n");
-		
-		if (imc <= 19) {
-			pesoIdeal = 22 * altura * altura;
-			System.out.println("O peso ideal para a sua altura é: " + pesoIdeal + " kgs.");}
-	    else
-	        if ((imc > 19) && (imc <=25)) {
-	            limiteInf = 19 * altura * altura;
-	            limiteSup = 25 * altura * altura;
-				System.out.println("O peso deve estar entre " + limiteInf + " kgs e os " + limiteSup + " kgs.");}
-	    else
-	        if ((imc > 25.0)&& (imc <=30)) {
-				pesoIdeal = 22 * altura * altura;
-				System.out.println("O peso ideal para a sua altura é: " + pesoIdeal + " kgs.");}
-		else {
-	            pesoIdeal = 22 * altura * altura;
-				System.out.println("O peso ideal para a sua altura é: " + pesoIdeal + " kgs.");}
-	
-	}
 }
 	
